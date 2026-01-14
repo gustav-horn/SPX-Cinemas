c53b5205273014a2ac7e042c705865fbff979efd
= <=>
2026-01-14 16:04:15 +1100

    filled out the about us page

--- view/aboutUs.php
+++ view/aboutUs.php
@@ -8,27 +8,34 @@
     include __DIR__ . '/../view/header.php';
 ?>
 <h1>About Us</h1>
-<h3>What we are all about</h3>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam deserunt, reiciendis ea ut alias voluptate fuga cum dolor. Quod assumenda minus cupiditate odio autem maxime doloribus praesentium id aut perferendis!
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit vitae minus perspiciatis, ad laborum hic inventore animi provident placeat! Accusamus earum officiis nulla at quas. Dolores voluptas iste quae praesentium.
-    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae, doloribus sint non laboriosam quod deserunt, maxime corrupti iste quis beatae tempora. Reiciendis, dolorum. Dignissimos eos, expedita doloremque dolorem quaerat est?
-    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod harum totam repudiandae exercitationem necessitatibus tempora illo atque hic facilis, sed fuga, non corrupti soluta assumenda itaque reprehenderit. Vero, exercitationem sed.
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex harum nobis eaque delectus, repellat labore ducimus doloribus perspiciatis nam nostrum eveniet ab optio consequatur totam quis debitis. Vitae, sequi perspiciatis.
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium molestiae, minus consectetur quas sit deserunt, quo earum harum tempore doloremque sint eos esse! Iure illum asperiores ut, quas repellat voluptas.
-    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consectetur necessitatibus excepturi architecto vero obcaecati veniam, aspernatur amet commodi officia! Eos eligendi perferendis in et necessitatibus vitae accusantium harum impedit similique.
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse autem, reiciendis asperiores ratione expedita magnam eum. Repudiandae accusamus explicabo dolores at sunt. Maiores iure aliquid architecto rerum nemo. Temporibus, facere?
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora atque veritatis quisquam nam harum architecto dignissimos mollitia nihil ipsum sapiente quaerat, corrupti, deleniti suscipit temporibus magni nulla eaque unde rem?
-    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis similique ipsam aperiam sed tempore deserunt, qui explicabo dolore possimus natus consectetur illum porro rerum optio nihil voluptatum, mollitia consequatur minus.
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus commodi aperiam, itaque vero iure expedita, aliquid repellat nemo distinctio odio tempore facere tenetur est doloremque. Repellendus repellat eius at asperiores?
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit ea sit natus sunt! Velit corrupti iure, iusto in laudantium mollitia molestiae repellendus blanditiis saepe rem voluptatibus praesentium dolorum? Enim, excepturi.
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque laborum rerum, id aut illum minima? Sit esse aliquam veritatis ducimus ad perspiciatis, quasi officiis ullam omnis illo id hic fugiat.
-    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error quam dicta ullam, hic perspiciatis, eos autem eveniet, mollitia explicabo ratione atque similique fugiat? Fugiat rerum, quos nam labore ipsam aliquid.
-    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati et impedit unde accusantium, maiores possimus sequi aut eaque illum aliquam! Aliquid sunt debitis mollitia, veniam dolores qui quam earum minima!
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt dolore officiis, molestias animi assumenda ipsam totam distinctio reprehenderit ipsum fugit ducimus quam dicta eaque autem velit accusantium sit placeat? Deserunt?
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure itaque est qui ipsam laudantium quis maxime hic consectetur fugit minus explicabo, saepe accusamus nam animi ratione, labore obcaecati ab non.
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus ea ducimus numquam doloremque officiis voluptate, et neque? Voluptatem similique dolores pariatur magni id, sed cum at, fugiat alias ipsam excepturi.
-    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Natus perspiciatis aliquid labore optio dolor ipsum explicabo, sunt, quibusdam delectus reiciendis doloremque enim cumque voluptate error dolorem voluptatum nobis sit iusto.
+
+<div class="row">
+    <div class="col">
+        <h3>What we are all about</h3>
+        <p>At SPX Cinemas, we believe in the magic of movies. We're more than just a place to watch films; we're a destination where stories come to life, shared experiences are created, and every visit feels special. Our mission is to bring people together through the power of storytelling, offering an exceptional cinema experience that combines cutting-edge technology with genuine hospitality.</p>
+        <span style="padding: 0.75%"></span>
+        <p>Founded on a passion for film and community, SPX Cinemas has grown from a single-screen theatre into a beloved local entertainment hub. We've stayed true to our core belief: that going to the movies should be an event worth remembering—a perfect blend of escape, entertainment, and comfort.</p>
+        <span style="padding: 0.75%"></span>
+    </div>
+    <div class="logo-container">
+        <img class="logo" src="assets\img\SPXCinemas_Logo.png" alt="The logo of SPX Cinemas">
+    </div>
+</div>
+
+<h3>What Makes Us Different</h3>
+<p>
+From the moment you walk through our doors, you'll notice the SPX difference. We've carefully designed every aspect of your visit:
 </p>
+<div class="col" style="margin-left: 1%">
+    <span style="padding: 0.75%"></span>
+    <p>Premium Viewing: Experience films as they were meant to be seen with our state-of-the-art projection and sound systems.</p>
+    <span style="padding: 0.75%"></span>
+    <p>Exceptional Comfort: Relax in our spacious, plush seating designed for maximum enjoyment throughout any feature-length film.</p>
+    <span style="padding: 0.75%"></span>
+    <p>Thoughtful Amenities: Enjoy our curated selection of snacks and beverages that go beyond the ordinary.</p>
+    <span style="padding: 0.75%"></span>
+    <p>Welcoming Atmosphere: Our friendly team is dedicated to making your visit seamless and memorable.</p>
+</div>
 
 <?php
     include __DIR__ . '/../view/footer.php';
