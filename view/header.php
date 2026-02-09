24f916bc9ee16edd3b7aa8ecf7b8820d42c7d7dd
= <=>
2026-02-09 19:52:26 +1100

    removed content-security-policy

--- view/header.php
+++ view/header.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="assets/img/favicon.png">
-    <meta http-equiv="content-security-policy" content="default-src 'self' www.youtube.com">
     <title>SPX Cinemas</title>
     <link rel="stylesheet" href="assets/css/style.css">
 </head>
