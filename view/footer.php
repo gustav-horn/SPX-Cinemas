9c0b74706c4575ee0ea338d8d65f67289a6c3565
= <=>
2026-03-19 09:56:59 +1100

    displays first and last names of the current user

--- view/footer.php
+++ view/footer.php
@@ -2,6 +2,9 @@
 <footer class="site-footer">
     <div class="container">
         <p>&copy; <?php echo date('Y'); ?> SPX Cinemas</p>
+        <?php if ($sessionManager->checkLoggedIn()): ?>
+            <div>Current User: <?= $sessionManager->getActiveUser()->firstName->decrypt() ?> <?= $sessionManager->getActiveUser()->lastName->decrypt() ?></div>
+        <?php endif ?>
     </div>
 </footer>
 </body>
